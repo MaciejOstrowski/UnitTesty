606eb7f77043eea7010cbca2bbd68c1d86b1ffc1
WW930\A701895 <maciej.ostrowski@atos.net>
2018-09-26 12:09:32 +0200

    UnitTests and Postman-requests API

---
+++ UNITTESTY/this/bootcamp-this/src/specs/this/index.spec.js
@@ -0,0 +1,184 @@
+describe('casting this to a temporary variable', () => {
+    it('should call the most outer context object property', function(){
+        // create the outerSayName function based on any of the below inner functions to return the "name" variable from the "outerContext" function context
+ 
+        function outerContext() {
+            function sayName() {
+                return this.name;
+            }
+           
+            var person1 = {
+                name: "John",
+                sayName: sayName
+            }
+ 
+            var person2 = {
+                name: "Zakk",
+                sayName: sayName
+            }
+ 
+            function outerSayName() {
+                return name;
+            }
+            var name = "Freddie";
+           
+            outerSayName();
+            expect(person1.sayName()).toEqual("John");
+            expect(person2.sayName()).toEqual("Zakk");
+            expect(outerSayName()).toEqual("Freddie");
+            // expect(this.outerSayName()).toEqual("Freddie");
+        }
+ 
+        outerContext();
+    })
+})
+ 
+describe('casting this to a temporary variable', () => {
+    it('should belong to the most outer context', function(){
+        // declare a variable myNumber of type number in the most outer "this" context object
+ 
+        this.myNumber = 5;
+        expect(this.myNumber).toEqual(5);
+ 
+    })
+    it('should be able to call the outer context from another context', function(){
+        // declare a variable myNumber of type number in the most outer "this" context object
+        // cast the context to a variable named "that" in a correct place, so it refers to the most outer "this" context object, when called from another context
+        this.myNumber = 45;
+        expect(this.myNumber).toEqual(45);
+        var that = this;
+        var myObj = {
+            delayedOutput: function(){
+                expect(this.myNumber).toEqual(undefined);
+                expect(that.myNumber).toEqual(45);
+            }
+        }
+ 
+        myObj.delayedOutput();
+    })
+})
+ 
+describe('using the call(), apply() and bind() methods to change the context', () => {
+    it('should use the call() method to change the context', function(){
+        function tellMeAboutYourself(name) {
+            if (this.alive) {
+                return name + " is " + this.age + " years old";
+            }
+            else {
+                return name + " would be " + this.age + " years old";
+            }
+        }
+ 
+        var person1 = {
+            name: "John",
+            age: 78,
+            alive: false,
+            tellMeAboutYourself: tellMeAboutYourself
+        }
+ 
+        var person2 = {
+            name: "Zakk",
+            age: 51,
+            alive: true,
+            tellMeAboutYourself: tellMeAboutYourself
+        }
+ 
+        this.name = "Freddie";
+        this.age = 72;
+        this.alive = false;
+ 
+        // use object identifiers and object methods only
+        expect(tellMeAboutYourself.call(person1, person1.name)).toEqual("John would be 78 years old");
+        expect(tellMeAboutYourself.call(person2, person2.name)).toEqual("Zakk is 51 years old");
+        expect(tellMeAboutYourself.call(this, this.name)).toEqual("Freddie would be 72 years old");
+        expect(tellMeAboutYourself.call(person1, person2.name)).toEqual("Zakk would be 78 years old");
+        expect(tellMeAboutYourself.call(this, person1.name)).toEqual("John would be 72 years old");
+        expect(person1.tellMeAboutYourself.call(this, this.name)).toEqual("Freddie would be 72 years old");
+        expect(person1.tellMeAboutYourself.call(person2, person2.name)).toEqual("Zakk is 51 years old");
+        expect(person2.tellMeAboutYourself.call(this, person2.name)).toEqual("Zakk would be 72 years old");
+    })
+ 
+    it('should use the apply() method to change the context', function(){
+        function tellMeAboutYourself(name) {
+            if (this.alive) {
+                return name + " is " + this.age + " years old";
+            }
+            else {
+                return name + " would be " + this.age + " years old";
+            }
+        }
+ 
+        var person1 = {
+            name: "John",
+            age: 78,
+            alive: false,
+            tellMeAboutYourself: tellMeAboutYourself
+        }
+ 
+        var person2 = {
+            name: "Zakk",
+            age: 51,
+            alive: true,
+            tellMeAboutYourself: tellMeAboutYourself
+        }
+ 
+        this.name = "Freddie";
+        this.age = 72;
+        this.alive = false;
+ 
+        // use object identifiers and object methods only
+        expect(tellMeAboutYourself.apply(person2, [person1.name])).toEqual("John is 51 years old");
+        expect(tellMeAboutYourself.apply(this, [person2.name])).toEqual("Zakk would be 72 years old");
+        expect(tellMeAboutYourself.apply(this, [this.name])).toEqual("Freddie would be 72 years old");
+        expect(tellMeAboutYourself.apply(person1, [person2.name])).toEqual("Zakk would be 78 years old");
+        expect(tellMeAboutYourself.apply(this, [person1.name])).toEqual("John would be 72 years old");
+        expect(person1.tellMeAboutYourself.apply(person2, [person1.name])).toEqual("John is 51 years old");
+        expect(person1.tellMeAboutYourself.apply(person2, [person2.name])).toEqual("Zakk is 51 years old");
+        expect(person2.tellMeAboutYourself.apply(person1, [person1.name])).toEqual("John would be 78 years old");
+    })
+ 
+    it('should use the bind() method to change the context', function(){
+        function tellMeAboutYourself(name) {
+            if (this.alive) {
+                return name + " is " + this.age + " years old";
+            }
+            else {
+                return name + " would be " + this.age + " years old";
+            }
+        }
+ 
+        var person1 = {
+            name: "John",
+            age: 78,
+            alive: false
+        }
+ 
+        var person2 = {
+            name: "Zakk",
+            age: 51,
+            alive: true
+        }
+ 
+        this.name = "Freddie";
+        this.age = 72;
+        this.alive = false;
+ 
+        // use object identifiers and object methods only
+ 
+        var myCallers = {
+            callerFunction1: tellMeAboutYourself.bind(person1),
+            callerFunction2: tellMeAboutYourself.bind(this),
+            callerFunction3: tellMeAboutYourself.bind(person2),
+            callerFunction4: tellMeAboutYourself.bind(this, person2.name),
+            callerFunction5: tellMeAboutYourself.bind(person1, this.name),
+            callerFunction6: tellMeAboutYourself.bind(person2, person2.name)
+        }
+ 
+        expect(myCallers.callerFunction1(person2.name)).toEqual("Zakk would be 78 years old");
+        expect(myCallers.callerFunction2(person1.name)).toEqual("John would be 72 years old");
+        expect(myCallers.callerFunction3(this.name)).toEqual("Freddie is 51 years old");
+        expect(myCallers.callerFunction4()).toEqual("Zakk would be 72 years old");
+        expect(myCallers.callerFunction5()).toEqual("Freddie would be 78 years old");
+        expect(myCallers.callerFunction6()).toEqual("Zakk is 51 years old");
+    })
+})
---
+++ UNITTESTY/unitTests/bootcamp-scopes-and-closures/src/specs/scopes-and-closures/index.spec.js
@@ -0,0 +1,90 @@
+describe('global scope - scopes and closures', () => {
+    it('should belong to the global scope', function(){
+        // declare a variable MyNumber of type number, that should be available in the global scope (outside the MyMathLibrary object)
+        // update the multiplyByTwo() method code to multiply the value of MyNumber by 2
+        // make sure the MyMathLibrary.multiplyByTwo() method has been called
+        var MyNumber = 5;
+ 
+        var MyMathLibrary = {
+            multiplyByTwo: function() {
+                return MyNumber = MyNumber*2;
+            }
+        }
+ 
+        spyOn(MyMathLibrary, "multiplyByTwo").and.callThrough();
+ 
+        expect(MyNumber).toEqual(5);
+ 
+        MyMathLibrary.multiplyByTwo(MyNumber);
+ 
+        expect(MyMathLibrary.multiplyByTwo).toHaveBeenCalled();
+        expect(MyNumber).toEqual(10);
+    })
+})
+ 
+describe('functional scope - scopes and closures', () => {
+    it('should belong to the function scope', function(){
+        // declare a variable MyNumber of type number (with an initial value of 9), that should be available in the global scope (outside the MyMathLibrary object)
+        // update the doMath() method code to make the MyNumber value equal 10 / hint: you just need to add one line of code
+        // make sure the MyMathLibrary.doMath() method has been called (with a proper parameter value)
+        // make sure the addSix() and reduceFive() functions are NOT available in the global scope
+        var MyNumber = 9;
+    
+        var MyMathLibrary = {
+            doMath: function(arg) {
+               MyNumber = MyMathLibrary.addSix(arg);
+               return MyNumber = MyMathLibrary.reduceFive(MyNumber);
+            },
+            addSix: (arg) =>{
+                return arg + 6;
+            },
+            reduceFive: (arg) =>{
+                return arg - 5;
+            }
+        }
+       
+        spyOn(MyMathLibrary, "doMath").and.callThrough();
+ 
+        // testing initial value
+        expect(MyNumber).toEqual(9);
+ 
+        MyMathLibrary.doMath(MyNumber);
+ 
+        expect(MyMathLibrary.doMath).toHaveBeenCalled();
+ 
+        // testing result value
+        expect(MyNumber).toEqual(10);
+        expect(typeof addSix).toEqual("undefined");
+        expect(typeof reduceFive).toEqual("undefined");
+    })
+ 
+    it('should belong to the function scope - IIFE', function(){
+        // declare a variable MyNumber of type number (with an initial value of 10), that should be available in the global scope (outside the MyMathLibrary object)
+        // DO NOT modify the MyMathLibrary.doMath() method code
+        // make sure the MyMathLibrary.doMath() method has NOT been called within the global scope
+        // call the doSomeMoreMath() function but make sure it is not poluting the global scope (use IIFE)
+        var MyNumber = 10;
+ 
+        var MyMathLibrary = {
+            doMath: function(arg) {
+                MyNumber = addSix(arg);
+            }
+        }
+ 
+        function addSix(arg) {
+            return arg + 6;
+        }
+ 
+        (function doSomeMoreMath(){
+            MyMathLibrary.doMath(MyNumber)
+        }())
+ 
+        spyOn(MyMathLibrary, "doMath").and.callThrough();
+ 
+        expect(MyMathLibrary.doMath).not.toHaveBeenCalled();
+ 
+        // testing result value
+        expect(MyNumber).toEqual(16);
+        expect(typeof doSomeMoreMath).toEqual("undefined");
+    })
+})
